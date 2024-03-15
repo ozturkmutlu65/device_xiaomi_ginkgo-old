7a60924f3d0a709d2abb89fde14b656517009369
ozturkmutlu65 <155987147+ozturkmutlu65@users.noreply.github.com>
2024-03-15 02:37:50 -0700

    Drop some Prebuilt Packages

--- device.mk
+++ device.mk
@@ -368,6 +368,11 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.secure_element@1.2.vendor
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
+
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/seccomp/codec2.vendor.ext.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/codec2.vendor.ext.policy \
