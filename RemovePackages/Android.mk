43656a654025d1e013c58b0df860cb6e8817f21f
ozturkmutlu65 <155987147+ozturkmutlu65@users.noreply.github.com>
2024-04-22 07:36:10 +0300

    Added Nuke Packages

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -5,6 +5,17 @@ LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
+    Chrome \
+    Chrome-Stub \
+    Drive \
+    Maps \
+    Photos \
+    PrebuiltGmail \
+    RecorderPrebuilt \
+    SafetyHubPrebuilt \
+    Videos \
+    Google \
+    YouTube \
     ViaBrowser \
     Auxio
 LOCAL_UNINSTALLABLE_MODULE := true
