1262316c67ab3743b8fd38a54588e2aab6766238
ozturkmutlu65 <155987147+ozturkmutlu65@users.noreply.github.com>
2024-04-23 19:44:24 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,5 +8,5 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_ginkgo.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_ginkgo-ap1a-user \
-    lineage_ginkgo-ap1a-userdebug
+    lineage_ginkgo-user \
+    lineage_ginkgo-userdebug
