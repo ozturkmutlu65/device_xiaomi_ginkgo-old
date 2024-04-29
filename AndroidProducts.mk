28b66697d6227ebc2d70b95782688d7cd9f46cce
ozturkmutlu65 <155987147+ozturkmutlu65@users.noreply.github.com>
2024-04-30 00:28:42 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,5 +8,5 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/bliss_ginkgo.mk
 
 COMMON_LUNCH_CHOICES := \
-    bliss_ginkgo-user \
-    bliss_ginkgo-userdebug
+    bliss_ginkgo-ap1a-user \
+    bliss_ginkgo-ap1a-userdebug
