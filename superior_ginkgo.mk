754eed3319575f335774b6f6722cdc22518ef638
ozturkmutlu65 <155987147+ozturkmutlu65@users.noreply.github.com>
2024-05-02 16:13:51 +0300

    Update superior_ginkgo.mk

--- superior_ginkgo.mk
+++ superior_ginkgo.mk
@@ -24,7 +24,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 
 # Inherit some common ProjectEverest stuff
-$(call inherit-product, vendor/superior/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Blur
 TARGET_ENABLE_BLUR := false
@@ -36,7 +36,7 @@ EVEREST_UDFPS_ANIMATIONS := false
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier
-PRODUCT_NAME := superior_ginkgo
+PRODUCT_NAME := lineage_ginkgo
 PRODUCT_DEVICE := ginkgo
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 8/8T
