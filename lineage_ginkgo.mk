1935bf80610d5e98d341ffc5f176311adfdcc36a
ozturkmutlu65 <155987147+ozturkmutlu65@users.noreply.github.com>
2024-05-15 23:27:03 -0700

    Update lineage_ginkgo.mk

--- lineage_ginkgo.mk
+++ lineage_ginkgo.mk
@@ -1,6 +1,5 @@
 #
-<<<<<<< HEAD
-# Copyright (C) Project-Everest
+# Copyright (C) LineageOS
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,11 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-=======
-# Copyright (C) 2021 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
->>>>>>> 355fa40 (ginkgo: Treewide Cleanup)
 #
 
 # Inherit framework first
@@ -29,12 +23,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from ginkgo device
 $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 
-<<<<<<< HEAD
-# Inherit some common ProjectEverest stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-RISING_PACKAGE_TYPE := "VANILLA_AOSP"
-WITH_GMS := false
+# Inherit some common LineageOS stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Blur
 TARGET_ENABLE_BLUR := false
@@ -44,12 +35,6 @@ EVEREST_UDFPS_ANIMATIONS := false
 
 # Boot Animation
 TARGET_BOOT_ANIMATION_RES := 1080
-=======
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-# Bootanimation Res
-TARGET_BOOT_ANIMATION_RES := 1080
 
 # EPPE
 TARGET_DISABLE_EPPE := true
@@ -57,18 +42,6 @@ TARGET_DISABLE_EPPE := true
 # Blur effect
 TARGET_ENABLE_BLUR := false
 
-# Matrixx Stuff
-MATRIXX_BUILD_TYPE := SHAWK
-MATRIXX_CHIPSET := SM6125
-MATRIXX_BATTERY := 4000mAh
-MATRIXX_DISPLAY :=  1080x2340
-MATRIXX_MAINTAINER := eklerismunir
-
-# Exclude prebuilt apps
-TARGET_EXCLUDES_AUDIOFX := true
-TARGET_EXCLUDES_AUXIO := true
-TARGET_EXCLUDES_VIA := true
->>>>>>> 355fa40 (ginkgo: Treewide Cleanup)
 
 # Device identifier
 PRODUCT_NAME := lineage_ginkgo
@@ -76,12 +49,4 @@ PRODUCT_DEVICE := ginkgo
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 8/8T
 PRODUCT_MANUFACTURER := Xiaomi
-<<<<<<< HEAD
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    RISING_CHIPSET="SM6125" \
-    RISING_MAINTAINER="mrgebesturtle"
-=======
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
->>>>>>> 355fa40 (ginkgo: Treewide Cleanup)
