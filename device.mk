739a80069ae198722e415981bc7ed24f94a6e8f4
ozturkmutlu65 <155987147+ozturkmutlu65@users.noreply.github.com>
2024-07-16 18:02:24 +0300

    ginkgo: Build Camera2

--- device.mk
+++ device.mk
@@ -90,6 +90,9 @@ PRODUCT_PACKAGES += \
     libpiex_shim \
     vendor.qti.hardware.camera.device@1.0.vendor
 
+PRODUCT_PACKAGES += \
+    Camera2
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
