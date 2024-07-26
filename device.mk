234d65236670e8b908b7a4618a367a302a7fe44d
eklerismunir <munir.nasibzade@proton.me>
2024-07-26 19:35:27 +0400

    ginkgo: Drop libldacBT_bco Signed-off-by: eklerismunir <munir.nasibzade@proton.me>

--- device.mk
+++ device.mk
@@ -75,7 +75,6 @@ PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     libldacBT_enc \
     libldacBT_abr \
-    libldacBT_bco \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
